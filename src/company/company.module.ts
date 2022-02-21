01df638fa4c4576f7013d954b8eae2cb56c8bd44
Lucas Guiss <lucasguissgusmao@gmail.com>
2022-02-21 20:30:53 -0300

    style(company-module): fix eslint identation

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -1,12 +1,8 @@
-import { Module } from "@nestjs/common";
-import { CompanyService } from "./company.service";
+import { Module } from '@nestjs/common';
+import { CompanyService } from './company.service';
 
 @Module({
-    providers: [
-        CompanyService
-    ],
-    exports: [
-        CompanyService
-    ]
+  providers: [CompanyService],
+  exports: [CompanyService],
 })
 export class CompanyModule {}
